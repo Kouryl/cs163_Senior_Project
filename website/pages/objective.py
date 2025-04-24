93f672b4f40697b313fa808d74c895a255b45d80
benjaminkzm <benjaminkhor21@gmail.com>
2025-04-24 01:29:38 -0700

    Updated project objectives

--- website/pages/objective.py
+++ website/pages/objective.py
@@ -5,9 +5,9 @@ dash.register_page(__name__, path="/objective")
 layout = html.Div([
     html.H2("Project Objective"),
     html.H3("There are 3 goals for this project: "),
-    html.P("1. To analyze and visualize the Growth of EVs through trends in EV energy consumption over the years."),
-    html.P("2. To examine whether the cost of charging an EV is cheaper than the cost of gasoline."),
-    html.P("3. To provide insights or predict future energy consumption from EV."),
+    html.P("1. Analyze historical and current data to visualize trends in EV energy consumption, illustrating the growth of EV adoption over time."),
+    html.P("2. Determine whether the cost of charging an EV provides a financial advantage over gasoline by examining electricity rates, gas prices, and overall cost per mile."),
+    html.P("3. Provide insights into potential future energy consumption by EVs, including forecasts that help stakeholders understand and prepare for shifts in demand."),
     html.H3("The project will use the following data sources:"),
     html.P("1. EVWatts Public Database 2019-2022"),
     html.P("2. EIA-861 Annual and Monthly Electric Power Industry Report"),
